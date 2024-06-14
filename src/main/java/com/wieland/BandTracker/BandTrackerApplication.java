238cd74d86da03e2611d06adba622361b6139f8e
Sebastian Wieland <wielandseb@gmail.com>
2024-06-14 14:04:27 +0200

    initial commit

---
+++ src/main/java/com/wieland/BandTracker/BandTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.wieland.BandTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wieland/BandTracker/BandTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wieland.BandTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
