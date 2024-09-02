441e40bb00b9f7f28a7ab25bbda7c6a73a167657
Sebastian Wieland <wielandseb@gmail.com>
2024-09-02 18:12:09 +0200

    Added CORS support for local development 🔄

--- src/main/java/com/ConcertJournalAPI/controller/BandEventController.java
+++ src/main/java/com/ConcertJournalAPI/controller/BandEventController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/BandTracker/events")
 public class BandEventController {
