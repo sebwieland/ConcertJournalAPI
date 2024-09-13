8ec000ccb02a23d4569c5b2c4b1dbe2c1d9afbd2
Sebastian Wieland <wielandseb@gmail.com>
2024-09-13 13:45:56 +0200

    Add user authentification

---
+++ src/main/java/com/ConcertJournalAPI/model/AppUser.java
@@ -0,0 +1,18 @@
+package com.ConcertJournalAPI.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/ConcertJournalAPI/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.ConcertJournalAPI.service;
+
+import com.ConcertJournalAPI.model.AppUser;
+import com.ConcertJournalAPI.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Fetch user from the repository
+        AppUser appUser = userRepository.findByUsername(username);
+        if (appUser == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        // Convert User to UserDetails
+        return org.springframework.security.core.userdetails.User.withUsername(appUser.getUsername())
+                .password(appUser.getPassword())
+                .roles("USER") // Set roles as needed
+                .build();
+    }
+
+
+}
