aeea8ff6ba7648ae0c661514efe1bff53ef91a62
Sebastian Wieland <wielandseb@gmail.com>
2024-09-18 16:49:46 +0200

    Refactor DataLoader to handle band events initialization 🎸

--- src/main/java/com/ConcertJournalAPI/DataLoader.java
+++ src/main/java/com/ConcertJournalAPI/DataLoader.java
@@ -1,23 +1,32 @@
 package com.ConcertJournalAPI;
 
 import com.ConcertJournalAPI.model.AppUser;
+import com.ConcertJournalAPI.model.BandEvent;
+import com.ConcertJournalAPI.repository.BandEventRepository;
 import com.ConcertJournalAPI.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final BandEventRepository bandEventRepository;
 
     @Autowired
     public DataLoader(UserRepository userRepository,
-                      PasswordEncoder passwordEncoder) {
+                      PasswordEncoder passwordEncoder,
+                      BandEventRepository bandEventRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.bandEventRepository = bandEventRepository;
     }
 
     @Override
@@ -32,9 +41,26 @@ public class DataLoader implements CommandLineRunner {
             userRepository.save(user);
 
             System.out.println("Default user created with username 'admin' and password 'adminpassword'");
-        }
-        else {
+        } else {
             System.out.println("Default user already exists");
         }
+
+        // IntStream.range(0, 10) // replace N with the desired number of dummy events
+        //        .forEach(i -> bandEventRepository.save(createDummyBandEvent(i)));
     }
+
+
+    private BandEvent createDummyBandEvent(int index) {
+        BandEvent dummyEvent = new BandEvent();
+        dummyEvent.setBandName("Bandname" + (index + 1));
+        dummyEvent.setPlace("here" + (index + 1));
+        dummyEvent.setDate(LocalDate.now());
+        dummyEvent.setRating(0);
+        dummyEvent.setComment("comment" + (index + 1));
+        dummyEvent.setCreationDate(Instant.now());
+        dummyEvent.setModificationDate(Instant.now());
+        return dummyEvent;
+    }
+
+
 }
