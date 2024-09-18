3b25eb1b254e05746b73c725212c2e41796abd66
Sebastian Wieland <wielandseb@gmail.com>
2024-09-18 16:21:44 +0200

    Implement security features and default admin user 💻

--- src/main/java/com/ConcertJournalAPI/configuration/SecurityConfiguration.java
+++ src/main/java/com/ConcertJournalAPI/configuration/SecurityConfiguration.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
@@ -14,16 +17,22 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 // Disable CSRF for simplicity (enable it in production)
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                )
+                .formLogin(withDefaults()
+                )
+
                 // Authorize requests
                 .authorizeHttpRequests(auth -> auth
-                        // Permit access to the registration and login endpoints
-                        .requestMatchers("/auth/**", "/users/register").permitAll()
-                        // All other endpoints require authentication
+                        .requestMatchers("/error", "/auth/**", "/register", "/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 // Use HTTP Basic Authentication (for simplicity)
-                .httpBasic();
+               .httpBasic(withDefaults());
 
         return http.build();
     }
--- src/main/java/com/ConcertJournalAPI/repository/UserRepository.java
+++ src/main/java/com/ConcertJournalAPI/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
+    Boolean existsAppUserByUsername(String username);
 }
