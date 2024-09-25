6e3db963933f85adc34734301db7ba97288d5bc5
Sebastian Wieland <wielandseb@gmail.com>
2024-09-25 11:08:50 +0200

    disbable BandEventServiceTest enabled CustomUserDetailsServiceTest

--- src/test/java/com/ConcertJournalAPI/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/ConcertJournalAPI/service/CustomUserDetailsServiceTest.java
@@ -16,7 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@Disabled
 public class CustomUserDetailsServiceTest {
 
     @InjectMocks
