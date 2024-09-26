0f30d815f764c321d79ec8fff8db079fc9453d70
Sebastian Wieland <wielandseb@gmail.com>
2024-09-26 13:19:18 +0200

    Refactor security configuration and tests 💻

--- src/main/java/com/ConcertJournalAPI/configuration/PasswordConfig.java
+++ src/main/java/com/ConcertJournalAPI/configuration/PasswordConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordConfig {
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- src/test/java/com/ConcertJournalAPI/configuration/SecurityConfigurationTest.java
+++ src/test/java/com/ConcertJournalAPI/configuration/SecurityConfigurationTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -26,10 +26,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(controllers = BandEventController.class)
 @AutoConfigureMockMvc
+@Import(PasswordConfig.class)
 public class SecurityConfigurationTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @MockBean
     private BandEventService bandEventService;
@@ -68,7 +72,6 @@ public class SecurityConfigurationTest {
         user.setEmail("user@example.com");
 
         // Hash the password
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(user.getPassword());
 
         // Verify that the hashed password is not equal to the original password
