75d72de349cbda9fb06a5d1c8797c83563989e67
Sebastian Wieland <wielandseb@gmail.com>
2024-09-27 16:51:39 +0200

    Implement JWT authentication and authorization 🚫

--- src/main/java/com/ConcertJournalAPI/configuration/SecurityConfiguration.java
+++ src/main/java/com/ConcertJournalAPI/configuration/SecurityConfiguration.java
@@ -1,24 +1,51 @@
 package com.ConcertJournalAPI.configuration;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Arrays;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
 
-    private AuthenticationManager authenticationManager;
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
@@ -27,13 +54,99 @@ public class SecurityConfiguration {
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(List.of("*")); // Allow all headers
+        corsConfig.setExposedHeaders(List.of("Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", corsConfig);
         return source;
     }
+
+    static SecretKey getSigningKey(String jwtSecret) {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    private class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+            String token = Jwts.builder()
+                    .subject((authentication.getName()))
+                    .issuedAt(new Date())
+                    .expiration(new Date((new Date()).getTime() + 86400000)) // 1 day
+                    .signWith(getSigningKey(jwtSecret))
+                    .compact();
+
+            response.setStatus(HttpStatus.OK.value());
+            response.setContentType("application/json");
+            response.getWriter().write("{\"token\":\"" + token + "\"}");
+        }
+    }
+
+    private static class AuthFailureHandler implements AuthenticationFailureHandler {
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        }
+    }
+
+    public static class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        }
+    }
+
+    public static class JwtAuthenticationFilter extends OncePerRequestFilter {
+        private final String jwtSecret;
+
+        public JwtAuthenticationFilter(@NonNull String jwtSecret) {
+            this.jwtSecret = jwtSecret;
+        }
+
+        @Override
+        protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+            String token = extractTokenFromRequest(request);
+            if (token != null) {
+                try {
+                    Claims claims = parseToken(token);
+                    authenticateUser(claims);
+                } catch (JwtException e) {
+                    handleInvalidToken(response, e);
+                }
+            }
+            filterChain.doFilter(request, response);
+        }
+
+        private String extractTokenFromRequest(HttpServletRequest request) {
+            String token = request.getHeader("Authorization");
+            if (token != null && token.startsWith("Bearer ")) {
+                return token.substring(7);
+            }
+            return null;
+        }
+
+        private Claims parseToken(String token) {
+            return Jwts.parser()
+                    .verifyWith(getSigningKey(jwtSecret))
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        }
+
+        private void authenticateUser(Claims claims) {
+            String username = claims.getSubject();
+            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        private void handleInvalidToken(HttpServletResponse response, JwtException e) {
+            // Log the error and return a meaningful error response
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -51,18 +164,23 @@ public class SecurityConfiguration {
 
                 .formLogin(form -> form
                         .usernameParameter("email")
+                        .successHandler(new AuthSuccessHandler())
+                        .failureHandler(new AuthFailureHandler())
                 )
-
-                //.addFilter(customUsernamePasswordAuthenticationFilter(authenticationManager))
-
 
                 // Authorize requests
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/error", "/auth/**", "/register", "/login").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated()
                 )
                 // Use HTTP Basic Authentication (for simplicity)
-               .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtSecret), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
