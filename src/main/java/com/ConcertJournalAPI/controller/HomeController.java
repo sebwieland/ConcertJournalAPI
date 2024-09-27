d07f71488087f0318b541e55ed93d12ed470c0d3
Sebastian Wieland <wielandseb@gmail.com>
2024-09-27 16:51:39 +0200

    Implement JWT authentication and authorization 🚫

--- src/main/java/com/ConcertJournalAPI/controller/HomeController.java
+++ src/main/java/com/ConcertJournalAPI/controller/HomeController.java
@@ -3,10 +3,12 @@ package com.ConcertJournalAPI.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HomeController {
 
     @PreAuthorize("isAuthenticated()")
