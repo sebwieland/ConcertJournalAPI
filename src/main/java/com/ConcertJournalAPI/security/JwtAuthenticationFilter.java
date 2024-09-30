2a613285d9fa75383fde6ca18073d3c032a4245d
Sebastian Wieland <wielandseb@gmail.com>
2024-09-30 13:41:10 +0200

    Refactor JwtAuthenticationFilter to use JwtUtils 🔄

--- src/main/java/com/ConcertJournalAPI/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ConcertJournalAPI/security/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.ConcertJournalAPI.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,51 +14,24 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Date;
-
-import static com.ConcertJournalAPI.configuration.SecurityConstants.AUTHORIZATION_HEADER;
-import static com.ConcertJournalAPI.configuration.SecurityConstants.BEARER_PREFIX;
-import static com.ConcertJournalAPI.security.JwtUtils.getSigningKey;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final String jwtSecret;
 
-    public JwtAuthenticationFilter(@NonNull String jwtSecret) {
-        this.jwtSecret = jwtSecret;
+    public JwtAuthenticationFilter() {
     }
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        String token = extractTokenFromRequest(request);
+        String token = JwtUtils.extractTokenFromRequest(request);
         if (token != null) {
             try {
-                Claims claims = parseToken(token);
+                Claims claims = JwtUtils.parseToken(token);
                 authenticateUser(claims);
             } catch (JwtException e) {
                 handleInvalidToken(response);
             }
         }
         filterChain.doFilter(request, response);
-    }
-
-    private String extractTokenFromRequest(HttpServletRequest request) {
-        String token = request.getHeader(AUTHORIZATION_HEADER);
-        if (token != null && token.startsWith(BEARER_PREFIX)) {
-            return token.substring(BEARER_PREFIX.length());
-        }
-        return null;
-    }
-
-    private Claims parseToken(String token) throws JwtException {
-        Claims claims = Jwts.parser()
-                .verifyWith(getSigningKey(jwtSecret))
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-        if (claims.getExpiration().before(new Date())) {
-            throw new JwtException("Token has expired");
-        }
-        return claims;
     }
 
     private void authenticateUser(Claims claims) {
