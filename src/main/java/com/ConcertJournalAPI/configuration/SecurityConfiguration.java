0e5b051acb37405ff11864eee3e71619688c3013
Sebastian Wieland <wielandseb@gmail.com>
2025-01-19 07:14:03 +0100

    Update same-site cookie policy.

--- src/main/java/com/ConcertJournalAPI/configuration/SecurityConfiguration.java
+++ src/main/java/com/ConcertJournalAPI/configuration/SecurityConfiguration.java
@@ -67,7 +67,6 @@ public class SecurityConfiguration {
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
 
                 // Enable CSRF protection
-
                 .csrf((csrf) -> csrf
                         .csrfTokenRepository(csrfTokenRepository())
                         .csrfTokenRequestHandler(new CookieCsrfTokenRequestHandler())
