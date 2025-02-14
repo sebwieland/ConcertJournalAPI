2363bf3aafcab1cd09d0e330487aaaa38d31475e
Sebastian Wieland <wielandseb@gmail.com>
2025-02-14 11:11:15 +0100

    Update timestamps with Hibernate annotations and adjust field types.

--- src/main/java/com/ConcertJournalAPI/ConcertJournalAPI.java
+++ src/main/java/com/ConcertJournalAPI/ConcertJournalAPI.java
@@ -2,12 +2,10 @@ package com.ConcertJournalAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableWebMvc
 public class ConcertJournalAPI {
 	public static void main(String[] args) {
